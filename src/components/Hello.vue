28a0e43a7e8b2e3bc2beec5503e545c963c0115a
Dandomanos <dandomanos@gmail.com>
2017-09-28 17:24:25 +0200

    starting

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,32 +2,44 @@
   <div class="hello">
     <a target="_blank" href="http://bulma.io/">
       <img src="./../assets/bulma-logo.png">
-      </a>
-    <p>
-      A modern CSS framework based on Flexbox by <b>Jeremy Thomas</b> (Frontend Designer from Bordeaux, France)<br />
-      <ul>
-        <li>
-            <a target="_blank" href="http://jgthms.com/">
-                <img src="./../assets/jeremy.jpg">
-            </a>
+    </a>
+    <section>
+        <p>
+        A modern CSS framework based on Flexbox by <b>Jeremy Thomas</b> (Frontend Designer from Bordeaux, France)<br />
+        <ul>
+            <li>
+                <a target="_blank" href="http://jgthms.com/">
+                    <img src="./../assets/jeremy.jpg">
+                </a>
+                </li>
+            <li>
+                <a target="_blank" href="https://github.com/jgthms">
+                    <img src="./../assets/github.svg">
+                </a>
             </li>
-        <li>
-            <a target="_blank" href="https://github.com/jgthms">
-                <img src="./../assets/github.svg">
-            </a>
-        </li>
-        <li>
-            <a target="_blank" href="https://twitter.com/jgthms?lang=es">
-                <img src="./../assets/twitter.svg">
-            </a>
-        </li>
-        <li>
-            <a target="_blank" href="http://caniuse.com/#feat=flexbox">
-                <img src="./../assets/caniuse.png">
-            </a>
-        </li>
-      </ul>
-    </p>
+            <li>
+                <a target="_blank" href="https://twitter.com/jgthms?lang=es">
+                    <img src="./../assets/twitter.svg">
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="http://caniuse.com/#feat=flexbox">
+                    <img src="./../assets/caniuse.png">
+                </a>
+            </li>
+        </ul>
+        </p>
+    </section>
+    <section>
+        <h2>Starting</h2>
+        <p class="code">
+            git clone https://github.com/Dandomanos/taller-bulma<br />
+            cd taller-bulma <br />
+            git checkout starting<br />
+            npm i <br />
+            npm run dev <br />
+        </p>
+    </section>
   </div>
 </template>
 
@@ -46,7 +58,9 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-
+section {
+    margin:4rem auto;
+}
 ul {
     list-style-type: none;
     padding: 0;
@@ -64,5 +78,15 @@ ul {
 
 a {
   color: #42b983;
+}
+.code {
+    font-family:monospace;
+    font-size: 1.5rem;
+    text-align:left;
+    border:1px dashed #ccc;
+    padding:1rem;
+    max-width:40rem;
+    margin:0 auto;
+    width:auto;
 }
 </style>
