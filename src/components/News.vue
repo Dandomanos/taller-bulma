20aed0bb9448cb03bc4c32dfbd8968ab4e102eb9
Dandomanos <dandomanos@gmail.com>
2017-10-02 15:58:52 +0200

    News add Columns

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-      News
+      <div class="columns">
+        <div class="column" v-for="(item, index) in News" :key="index">
+          <h1 class="title">{{item.title}}</h1>
+          <figure class="image is-square content">
+            <img :src="item.image" alt="">
+          </figure>
+          <p>{{item.content}}</p>
+        </div>
+      </div>
   </div>
 </template>
 <script>
+import News from '../data/news.json'
 export default {
-  
+    data() {
+        return {
+            News
+        }
+    }
 }
 </script>
 
