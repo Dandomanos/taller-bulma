6d5f694375e8f2357750e003e77dcf5d0e500c14
Dandomanos <dandomanos@gmail.com>
2017-10-03 16:02:49 +0200

    nesting media-object with form

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <article class="media">
+        <article class="media box">
             <figure class="media-left">
                 <div class="image is-64x64">
                     <img :src="News[0].image" alt="">
@@ -13,9 +13,84 @@
                         {{News[0].content}}
                     </p>
                 </div>
+                <article class="media">
+                    <figure class="media-left">
+                        <div class="image is-64x64">
+                            <img :src="News[1].image" alt="">
+                        </div>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>{{News[0].title}}</strong><br>
+                                {{News[0].content}}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <a class="delete is-large"></a>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <div class="image is-64x64">
+                            <img :src="News[2].image" alt="">
+                        </div>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>{{News[0].title}}</strong><br>
+                                {{News[0].content}}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <a class="delete is-large"></a>
+                    </div>
+                </article>
+                <article class="media">
+                    <figure class="media-left">
+                        <div class="image is-64x64">
+                            <img :src="News[3].image" alt="">
+                        </div>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>{{News[0].title}}</strong><br>
+                                {{News[0].content}}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <a class="delete is-large"></a>
+                    </div>
+                </article>
             </div>
             <div class="media-right">
                 <a class="delete is-large"></a>
+            </div>
+        </article>
+        <article class="media box   ">
+            <figure class="media-left">
+                <div class="image is-64x64">
+                    <img :src="News[0].image" alt="">
+                </div>
+            </figure>
+            <div class="media-content">
+                <div class="field">
+                    <p class="control">
+                        <textarea name="" class="textarea" placeholder="Escribe tu mensaje"></textarea>
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control">
+                        <button class="button is-info is-pulled-right">
+                        Enviar
+                        </button>
+                    </p>
+                </div>
             </div>
         </article>
   </div>
