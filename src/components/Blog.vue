9e9e312f136d35bf0c1be2e5959dfa39856ab3ba
Dandomanos <dandomanos@gmail.com>
2017-10-04 11:04:01 +0200

    Add Modal simulte post message

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -67,6 +67,24 @@
                         <a class="delete is-large"></a>
                     </div>
                 </article>
+                <article class="media" v-for="(post, index) in replys" :key="index">
+                    <figure class="media-left">
+                        <div class="image is-64x64">
+                            <img :src="post.image" alt="">
+                        </div>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>{{post.title}}</strong><br>
+                                {{post.content}}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <a class="delete is-large"></a>
+                    </div>
+                </article>
             </div>
             <div class="media-right">
                 <a class="delete is-large"></a>
@@ -81,26 +99,75 @@
             <div class="media-content">
                 <div class="field">
                     <p class="control">
-                        <textarea name="" class="textarea" placeholder="Escribe tu mensaje"></textarea>
+                        <input name="" class="input" placeholder="Escribe un enunciado" v-model="title"></textarea>
                     </p>
                 </div>
                 <div class="field">
                     <p class="control">
-                        <button class="button is-primary is-pulled-right">
+                        <textarea name="" class="textarea" placeholder="Escribe tu mensaje" v-model="content"></textarea>
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control">
+                        <button
+                            class="button is-primary is-pulled-right"
+                            :class="{'is-loading':loading, 'is-static':!dirty}"
+                            @click="postMessage"
+                        >
                             Enviar
                         </button>
                     </p>
                 </div>
             </div>
         </article>
+        <modal :class="{'is-active':modalActive}" @close="closeModal"></modal>
   </div>
 </template>
 <script>
 import News from '../data/news.json'
+import Modal from '@/components/Modal'
 export default {
     data() {
         return {
-            News
+            News,
+            modalActive:false,
+            loading:false,
+            content:'',
+            title:'',
+            replys:[],
+        }
+    },
+    components: {
+        Modal
+    },
+    methods: {
+        postMessage() {
+            this.loading = true
+            setTimeout(this.showModal,1000)
+        },
+        showModal() {
+            this.modalActive = true
+            this.loading = false
+            this.replys.push(this.post)
+            this.clearForm()
+        },
+        closeModal() {
+            this.modalActive = false
+        },
+        clearForm() {
+            this.content = this.title = ''
+        }
+    },
+    computed: {
+        dirty() {
+            return this.content !== '' && this.title !== ''
+        },
+        post() {
+            return {
+                image: this.News[0].image,
+                title: this.title,
+                content: this.content,
+            }
         }
     }
 }
---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="modal">
+		<div class="modal-background"></div>
+		<div class="modal-content">
+			<div class="box">
+				Mensaje enviado correctamente
+			</div>
+		</div>
+		<button class="modal-close is-large" @click="close"></button>
+	</div>
+</template>
+<script>
+export default {
+	methods: {
+		close(){
+			console.log('close')
+			this.$emit('close')
+		}
+	}
+}
+</script>
+
