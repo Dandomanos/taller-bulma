24d4e140d74d3da9c43c85a9850fbc60bfa65b35
Dandomanos <dandomanos@gmail.com>
2017-10-04 11:58:39 +0200

    notification modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,11 +2,16 @@
 	<div class="modal">
 		<div class="modal-background"></div>
 		<div class="modal-content">
-			<div class="box">
+			<!-- <div class="box">
 				Mensaje enviado correctamente
-			</div>
+			</div> -->
+			<div class="notification is-primary">
+				Mensaje enviado correctamente
+			<a href="" class="delete" @click.prevent="close"></a>
 		</div>
-		<button class="modal-close is-large" @click="close"></button>
+	</div>
+		
+		<!-- <button class="modal-close is-large" @click="close"></button> -->
 	</div>
 </template>
 <script>
